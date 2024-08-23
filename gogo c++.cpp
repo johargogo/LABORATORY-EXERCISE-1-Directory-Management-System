9232e2401e72ba008aa7b09c635a2f49eef7b140
johargogo <gogodatujohar@gmail.com>
2024-08-23 20:30:04 +0800

    Add linecode

--- gogo c++.cpp
+++ gogo c++.cpp
@@ -15,5 +15,10 @@ void CRDirectory(){
     std::cout << "\nEnter the directory name: ";
     std::string dirName;
     std::cin >> dirName;
+if (_mkdir(dirName.c_str()) == 0) {
+        std::cout << "\nDirectory '" << dirName << "' Successfully Created\n";
+    } else {
+        std::cout << "\nFailed to Create Directory '" << dirName << "'\n";
+}
 
    
